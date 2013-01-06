9c1ab5280497f9cea9fd2255df4411fd23df3244
Łukasz Drynkowski <lukasz.drynkowski@gmail.com>
2013-01-06 20:25:57 +0100

    Structure + gitignore

---
+++ HowThingsWorks.System/GuidTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HowThingsWorks.System
+{
+    class GuidTest
+    {
+        [Test]
+        public void CreatingNewGUIDs()
+        {
+            Guid empty = new Guid();
+            Assert.AreEqual(Guid.Empty, empty);
+
+            Guid notEmpty = Guid.NewGuid();
+            Assert.AreNotEqual(Guid.Empty, notEmpty);
+        }
+
+        [Test]
+        public void CreatingNewGUIDsFromStrings()
+        {
+            var noDashes = new Guid("030B4A821B7C11CF9D5300AA003C9CB6");
+            var withDashes = new Guid("030B4A82-1B7C-11CF-9D53-00AA003C9CB6");
+
+            Assert.AreEqual(noDashes, withDashes);
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(FormatException))]
+        public void CreatingGuidWithTooManyDashesThrowsException()
+        {
+            var withManyDashes = new Guid("030B-4A82-1B7C-11CF-9D53-00AA-003C-9CB6");
+
+            Assert.Fail("No expection thrown");
+        }
+    }
+}
